23e6b9405571c4c42d18c3f0433a9d9758386fbe
Bernd Janzen <103428887+manjou@users.noreply.github.com>
2024-04-09 16:16:52 +0200

    myFlix-Angular-client Ex. 6.4 v.1.8 favMovie functionality working, user-profile not working. dont get user info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'profile', component: UserProfileComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
